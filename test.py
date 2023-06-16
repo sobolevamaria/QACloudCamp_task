82d4f83e4bcaea39d267de9c84030da577286c91
Maria <bumburumka@gmail.com>
2023-06-16 23:16:08 +0400

    improved tests

--- test.py
+++ test.py
@@ -15,8 +15,8 @@ class TestPostAPI:
     id = 1
 
     response = requests.get(f"{base_url}/posts/{id}")
-    jsonschema.validate(instance=response.json(), schema=post_schema)
     assert response.status_code == 200
+    jsonschema.validate(instance=response.json(), schema=post_schema)
     
   def test_delete_post(self):
     body = self.get_post_body()
@@ -32,8 +32,24 @@ class TestPostAPI:
     body = self.get_post_body()
     
     response = requests.post(f"{base_url}/posts", json=body)
-    jsonschema.validate(instance=response.json(), schema=post_schema)
     assert response.status_code == 201
+    jsonschema.validate(instance=response.json(), schema=post_schema)
+    jsonObj = response.json()
+    id = jsonObj.get("id")
+
+    response = requests.get(f"{base_url}/posts/{id}")
+
+    # fails for now
+    assert response.status_code == 200
+    jsonschema.validate(instance=response.json(), schema=post_schema)
+    jsonObj = response.json()
+    assert jsonObj.get("id") == id
+
+    assert jsonObj.get("userId") == body.get("userId")
+
+    assert jsonObj.get("title") == body.get("title")
+
+    assert jsonObj.get("body") == body.get("body")
     
   def get_post_body(self):
     body = {
